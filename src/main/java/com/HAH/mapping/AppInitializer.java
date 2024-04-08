5c41212c4a3c7af746093d9c3080fb7c85046cdb
hhtun137 <htunaunghlaing199@gmail.com>
2024-04-08 23:19:39 +0630

    switch javaBase Style into xml style

--- src/main/java/com/HAH/mapping/AppInitializer.java
+++ src/main/java/com/HAH/mapping/AppInitializer.java
@@ -1,22 +1,22 @@
-package com.HAH.mapping;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { RootConfig.class };
-	}
-
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] { ServletConfig.class };
-	}
-
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
-	}
-
-}
+//package com.HAH.mapping;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//	@Override
+//	protected Class<?>[] getRootConfigClasses() {
+//		return new Class<?>[] { RootConfig.class };
+//	}
+//
+//	@Override
+//	protected Class<?>[] getServletConfigClasses() {
+//		return new Class<?>[] { ServletConfig.class };
+//	}
+//
+//	@Override
+//	protected String[] getServletMappings() {
+//		return new String[] { "/" };
+//	}
+//
+//}
