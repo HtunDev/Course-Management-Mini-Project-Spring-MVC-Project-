231bdf77c3409509f8bc6de419fb4a629070b65c
hhtun137 <htunaunghlaing199@gmail.com>
2024-04-10 14:21:35 +0630

    adding spring jdbc

--- src/main/java/com/HAH/mapping/model/service/CourseCodeGenerator.java
+++ src/main/java/com/HAH/mapping/model/service/CourseCodeGenerator.java
@@ -1,13 +1,13 @@
-package com.HAH.mapping.model.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class CourseCodeGenerator {
-
-	private int id;
-
-	public int next() {
-		return ++id;
-	}
-}
+//package com.HAH.mapping.model.service;
+//
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CourseCodeGenerator {
+//
+//	private int id;
+//
+//	public int next() {
+//		return ++id;
+//	}
+//}
--- src/main/java/com/HAH/mapping/model/service/CourseService.java
+++ src/main/java/com/HAH/mapping/model/service/CourseService.java
@@ -1,49 +1,57 @@
 package com.HAH.mapping.model.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.HAH.mapping.model.dto.Course;
-import com.HAH.mapping.model.dto.Level;
 
 @Service
 public class CourseService {
 
-	private List<Course> coursesRepo;
-
-	@Autowired
-	private CourseCodeGenerator codeGenerator;
-
-	public CourseService() {
-		coursesRepo = new ArrayList<>();
-	}
-
-	@PostConstruct
-	public void init() {
-		create(new Course("Java Basic", 4, Level.Basic, 200000));
-		create(new Course("Spring", 3, Level.Advance, 180000));
-		create(new Course("Angular", 4, Level.Intermediate, 150000));
-		create(new Course("React", 4, Level.Intermediate, 200000));
-		create(new Course("Flutter", 4, Level.Basic, 180000));
-	}
-
 	public int create(Course c) {
-		var id = codeGenerator.next();
-		c.setId(id);
-		coursesRepo.add(c);
-		return id;
+		return 0;
 	}
 
 	public Course findById(int id) {
-		return coursesRepo.stream().filter(c -> c.getId() == id).findAny().orElse(null);
+		return null;
 	}
 
 	public List<Course> getAll() {
-		return List.copyOf(coursesRepo);
+		return null;
 	}
+
+//	This is manual
+//	private List<Course> coursesRepo;
+//
+//	@Autowired
+//	private CourseCodeGenerator codeGenerator;
+//
+//	public CourseService() {
+//		coursesRepo = new ArrayList<>();
+//	}
+//
+//	@PostConstruct
+//	public void init() {
+//		create(new Course("Java Basic", 4, Level.Basic, 200000));
+//		create(new Course("Spring", 3, Level.Advance, 180000));
+//		create(new Course("Angular", 4, Level.Intermediate, 150000));
+//		create(new Course("React", 4, Level.Intermediate, 200000));
+//		create(new Course("Flutter", 4, Level.Basic, 180000));
+//	}
+
+//	public int create(Course c) {
+//		var id = codeGenerator.next();
+//		c.setId(id);
+//		coursesRepo.add(c);
+//		return id;
+//	}
+//
+//	public Course findById(int id) {
+//		return coursesRepo.stream().filter(c -> c.getId() == id).findAny().orElse(null);
+//	}
+//
+//	public List<Course> getAll() {
+//		return List.copyOf(coursesRepo);
+//	}
 }
